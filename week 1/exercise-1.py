c1a6de9da200d012f11d3101ff0a08518ca033b0
Pavel Petrukhin <petrukhp@tcd.ie>
2021-10-19 14:14:32 +0100

    Update exercise-1.py

--- week 1/exercise-1.py
+++ week 1/exercise-1.py
@@ -2,6 +2,37 @@
     Defining functions. Please note we do not have to separate area or perimeter logic
     into separate functions, the program would still be correct. However, it is a best practice
     to separate self-contained functionality (commonly referred to as separation of concerns).
+    
+    
+    Quick note about functions:
+        
+        def func(param1, param2):
+            return param1 + param2
+        
+        defines a function called "func" which takes in 2 parameters and returns their sum.
+        
+        When the program execution hits the return statement it exits the function and
+        sends the return value to the place in the program where the function was called.
+        
+        For example, 
+        
+        a = func(10, 20) -> after this statement a will be equal to 30, since func returns the sum.
+        Essentially func(10,20) is substituted with the actual value (30), so it is equivalent to saying
+        a = 30
+        
+        print(func(10,20)) -> same idea here, it is equivalent to print(30)
+        
+        We may pass variables into the function as parameters and the variable names DO NOT 
+        have to match the parameter names, for example:
+        
+        num1 = 10
+        num2 = 20
+        print(func(num1, num2)) is transferred into print(func(10, 20)) and finally into print(30)
+        
+        We may have multiple return statements in the program, where each return statement 
+        corresponds to a particular case in the conditional execution, see examples in week 2.
+        
+        
 """
 def find_area(w, h):
     return w * h
